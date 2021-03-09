b73a75244b85a93c0b4bf9b2b322685900222604
ImTheCoder20 <65062215+ImTheCoder20@users.noreply.github.com>
2021-03-09 20:54:16 +0530

    1
    Moving code into GitHub

---
+++ index.js
@@ -0,0 +1,19 @@
+function inviteRedirect() {
+  window.location.href =
+    "https://discord.com/api/oauth2/authorize?client_id=810777004827344916&permissions=8&scope=bot";
+}
+
+function supportRedirect() {
+  window.location.href = "#";
+}
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  const x = document.getElementById("darkmode");
+  if (x.innerHTML === "Dark Mode") {
+    x.innerHTML = "Light Mode";
+  } else {
+    x.innerHTML = "Dark Mode";
+  }
+}
